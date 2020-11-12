f0b949d9a11836b2dcd5bef0aac9b912f3cd31b5
SabihBagci <sabihbagci@users.noreply.github.com>
2020-11-12 12:52:48 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -14,17 +14,14 @@ def add_handler():
         result = tk.Label(container_frame, text="Entering", height = 5, width = 15).grid(column=2, row=3)
 
 value1 = tk.IntVar()
-value1_entry = tk.Entry(container_frame, width= 10, textvariable=value1)
+value1_entry = tk.Entry(container_frame, width= 10)
 value1_entry.grid(column=2, row=4)
-
 
 var1 = tk.IntVar()
 c1 = tk.Checkbutton(container_frame, text='Remember Me',variable=var1, onvalue=1, offvalue=0)
 c1.grid(column=2, row=5)
 
-
 enter_button = tk.Button(container_frame, text="ENTER GAME!", command=add_handler)
 enter_button.grid(column=2, row=6, columnspan=2)
-
 
 win.mainloop()
