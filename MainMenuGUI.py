1368f9780986c1fe32c501fcf33e4c3396b880d9
SabihBagci <sabihbagci@users.noreply.github.com>
2020-11-12 13:46:50 +0300

    Update MainMenuGUI.py

--- MainMenuGUI.py
+++ MainMenuGUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 win = tk.Tk()
 win.title("Liars Dice 2020")
@@ -6,10 +7,6 @@ win.geometry("1000x550+100+100")
 
 container_frame = tk.LabelFrame(win, text="MAIN MENU")
 container_frame.grid(column=1, row=1, padx=425, pady=150)
-
-#def add_handler():
-#        result = tk.Label(container_frame, text="Entering", height = 5, width = 15).grid(column=2, row=3)
-
 
 createlobby_button = tk.Button(container_frame, text="CREATE LOBBY")
 createlobby_button.grid(column=1, row=1, columnspan=2)
@@ -29,7 +26,11 @@ startgame_button.grid(column=1, row=3, columnspan=2)
 settings_button = tk.Button(container_frame, text="SETTINGS")
 settings_button.grid(column=1, row=4, columnspan=2)
 
-credits_button = tk.Button(container_frame, text="CREDITS")
+def showCreditsMsg():  
+    messagebox.showinfo('Liars Dice 2020', 'ALL RIGHTS RESERVED, 2020, \n *MostFucKing GUI_App CO.*')
+ 
+credits_button = tk.Button(container_frame, text="CREDITS", command=showCreditsMsg)
 credits_button.grid(column=1, row=5, columnspan=2)
+
 
 win.mainloop()
