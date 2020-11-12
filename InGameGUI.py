18ac32389974662ff13b677c1bc40dec272cd4f4
SabihBagci <sabihbagci@users.noreply.github.com>
2020-11-12 14:29:51 +0300

    Create InGameGUI.py

---
+++ InGameGUI.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+win = tk.Tk()
+win.title("Liars Dice 2020")
+win.geometry("1000x550+100+100")
+
+#there will be play screen which include dices and other in-game components.
+
+win.mainloop()
