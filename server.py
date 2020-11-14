29c0867b9a7e6e465003018bffceaa95f034435b
Mustafa Candan <iletisimcandan@gmail.com>
2020-11-14 05:00:05 +0300

    Add yaml file

--- server.py
+++ server.py
@@ -1,56 +1,59 @@
+# packages
 import socket
-import argparse
 import threading
 import time
-import weakref
+import yaml
+# modules
 from game import Game
 from connection import Connection
 from commands import read
 
-parser = argparse.ArgumentParser( description="Liars Dice Server")
-parser.add_argument('--host', metavar='host', type=str, nargs='?', default='127.0.0.1')
-parser.add_argument('--port', metavar='port', type=int, nargs='?', default=5001)
-args = parser.parse_args()
+credentials = yaml.safe_load(open('./credentials.yml'))
+host = credentials['server']['host']
+port = credentials['server']['port']
 
+# socket initilization
 sck = socket.socket()
 sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-print(f"Server running on: {args.host} and port: {args.port}")
-
 try:
-    sck.bind((args.host, args.port))
+    # listening port
+    sck.bind((host, port))
     sck.listen(60)
+    print(f"Server running on: {host} and port: {port}")
 except Exception as e:
     raise SystemExit(
-        f"We could not bind the server on host: {args.host} to port: {args.port}, because: {e}")
+        f"We could not bind the server on host: {host} to port: {port}, because: {e}")
 
 def on_new_client(client, connection):
+    '''This function is called once on every new connection is made'''
+    
     cnn = Connection(ip=connection[0], port=connection[1])
     print(f"The new connection was made from IP: {cnn.ip}!\nAll conenctions are listed:")
     print('\n'.join([f'\tip:{c.ip} : port:{c.port}' for c in Connection.get_all()]))
 
+    # accepts incoming data from client
     while True:
-        msg = client.recv(64)
-        if msg.decode() == 'exit':
+        data = client.recv(64)
+        command = read(data)
+        
+        if command == 'exit':
             break
-        else:
-            read(msg.decode())
-        print(f"The client said: {msg.decode()}")
-        # reply = f"You told me: {msg.decode()}"
-        # client.sendall(reply.encode('utf-8'))
-    print(
-        f"The client from ip: {cnn.ip}, and port: {cnn.port}, has gracefully diconnected!")
+        
+    print(f"The client from ip: {cnn.ip}, and port: {cnn.port}, has gracefully diconnected!")
     client.close()
 
 
 while True:
     try:
+        # accepts new connections
         client, ip = sck.accept()
+        # start new thread for new connection
         threading._start_new_thread(on_new_client, (client, ip))
     except KeyboardInterrupt:
         print(f"Gracefully shutting down the server!")
+        break
     except Exception as e:
         print(f"Well I did not anticipate this: {e}")
-    time.sleep(0.5)
 
 sck.close()
